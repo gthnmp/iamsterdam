b8ef2151a3e7d7d5f99733dfc64d93914b139438
Gathan Mahesa <gathan.mahesa@gmail.com>
2023-09-06 18:52:08 +0700

    fix typo

--- app/components/ui/StoryCard.tsx
+++ app/components/ui/StoryCard.tsx
@@ -10,7 +10,7 @@ const StoryCard = ({title, src, date}:StoryCardProps) => {
   return(
     <div className="w-[25rem] h-32 flex gap-4">
       <div className="h-20 w-auto aspect-square bg-red-700 rounded-md overflow-hidden">
-        <Image src={src} alt="kontol" className='w-full h-full object-cover'/>
+        <Image src={src} alt="kontol" width={1280} height={1920} className='w-full h-full object-cover'/>
       </div>
       <div className="flex flex-col">
         {date && <h2 className="text-sm">{date}</h2>}
