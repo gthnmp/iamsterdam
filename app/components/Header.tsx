16191bc550a35c709d1b26c6691cb0023f174cf5
Gathan Mahesa <gathan.mahesa@gmail.com>
2023-09-07 10:03:02 +0700

    Refactoring

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,24 @@
-const content = {
-  header: {
-    title: ["Discover", "Amsterdam"],
-    paragraph: "I amsterdam is the official guide to everything Amsterdam. Find all the resources you need for the perfect trip, look up essential information on living, studying and working in the Amsterdam Area, and learn how to start a business in our beautiful city",
-    heroImage: "/assets/hero.webp"
-  }
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+
+interface Headline {
+  title : string[];
+  paragraph : string;
+  heroImageSrc: string | StaticImageData
+  heroImageAlt: string;
 }
-const Header = () => {
+
+interface Props {
+  content : Headline
+}
+
+const Header: React.FC<Props> = ({content}) => {
   return (
     <header className="w-full h-screen flex font-sans gap-10 px-4 py-4 my-8 border-b-neutral-200 border-b-2 ">
       <div className="w-1/2 h-full relative">
         <div className="w-full h-3/4 bg-red-600 rounded-md"/>
         <div className="w-full h-full absolute top-0 p-8 flex flex-col gap-2 ">
-          <img src={content.header.heroImage} className="w-full h-full object-cover rounded-md"/>
+          <Image src={content.heroImageSrc} alt={content.heroImageAlt} width={1280} height={1920} className="w-full h-full object-cover rounded-md"/>
           <p className="text-xs text-neutral-700">| Photography of Van Gogh Museum</p>
         </div>
       </div>
@@ -19,11 +26,11 @@ const Header = () => {
       <div className='w-1/2 h-full flex justify-center px-20'>
         <div className="flex flex-col gap-8">
           <div className='flex flex-col gap-2'>
-            <h1 className="font-bold text-red-600 text-6xl">{content.header.title[0]}</h1>
-            <h1 className="font-bold text-6xl">{content.header.title[1]}</h1>
+            <h1 className="font-bold text-red-600 text-6xl">{content.title[0]}</h1>
+            <h1 className="font-bold text-6xl">{content.title[1]}</h1>
           </div>
           <p className="text-neutral-700">
-            {content.header.paragraph}
+            {content.paragraph}
           </p>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Header from "./components/Header";
-import News from "./components/MainArticle";
+import MediumSizedGroup from './components/ui/group/MediumSizedGroup';
+import LargeSizedGroup from './components/ui/group/LargeSizedGroup';
+import SmallSizedGroup from './components/ui/group/SmallSizedGroup';
+import InstagramSection from './components/ui/InstagramSection';
+import homeContents from './contents/home.json'
 
 const Home = () => {
+  const { headline, happeningContent, amsterdamAreasContent, stories, discoverAmsterdam } = homeContents
   return (
     <div className='w-screen h-full overflow-x-hidden'>
-      <Header />
-      <News />
+      <Header content={headline}/>
+      <article className='w-screen h-full px-4'>
+        <MediumSizedGroup content={happeningContent}/>
+        <LargeSizedGroup content={amsterdamAreasContent}/>
+        <SmallSizedGroup content={stories} />
+        <SmallSizedGroup content={discoverAmsterdam} gridSize={3} />
+        <InstagramSection />
+      </article>
     </div>
   );
 };
