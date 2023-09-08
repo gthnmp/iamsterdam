eb9e09d0343f4fd5c93fd89565712050c5954670
Gathan Mahesa <gathan.mahesa@gmail.com>
2023-09-08 10:49:14 +0700

    Dynamic route codebase

---
+++ app/[page]/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "../components/Header";
+import MediumSizedGroup from '../components/ui/group/MediumSizedGroup';
+import LargeSizedGroup from '../components/ui/group/LargeSizedGroup';
+import SmallSizedGroup from '../components/ui/group/SmallSizedGroup';
+import PromotionSection from '../components/ui/InstagramSection';
+
+const typeToComponent = {
+  Header: Header,
+  PromotionSection: PromotionSection,
+  SmallSizedGroup: SmallSizedGroup,
+  MediumSizedGroup: MediumSizedGroup,
+  LargeSizedGroup: LargeSizedGroup,
+};
+
+export default async function Page({ params }: { params: { page: string } }) {
+  const { page } = params;
+  const content_data = await import(`../contents/${page}.json`);
+
+  return (
+    <div className='w-screen h-full overflow-x-hidden px-4'>
+      {content_data.default.map((section) => {
+        const Component = typeToComponent[section.type];
+        if (!Component) {
+          return null;
+        }
+
+        return (
+          <Component
+            key={section.name}
+            content={section.content}
+          />
+        );
+      })}
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,35 @@
+import React from "react";
 import Header from "./components/Header";
 import MediumSizedGroup from './components/ui/group/MediumSizedGroup';
 import LargeSizedGroup from './components/ui/group/LargeSizedGroup';
 import SmallSizedGroup from './components/ui/group/SmallSizedGroup';
-import InstagramSection from './components/ui/InstagramSection';
-import homeContents from './contents/home.json'
+import PromotionSection from './components/ui/InstagramSection';
+import homeContents from './contents/home.json'; 
+
+const typeToComponent = {
+  Header: Header,
+  PromotionSection: PromotionSection,
+  SmallSizedGroup: SmallSizedGroup,
+  MediumSizedGroup: MediumSizedGroup,
+  LargeSizedGroup: LargeSizedGroup,
+};
 
 const Home = () => {
-  const { headline, happeningContent, amsterdamAreasContent, stories, discoverAmsterdam } = homeContents
   return (
-    <div className='w-screen h-full overflow-x-hidden'>
-      <Header content={headline}/>
-      <article className='w-screen h-full px-4'>
-        <MediumSizedGroup content={happeningContent}/>
-        <LargeSizedGroup content={amsterdamAreasContent}/>
-        <SmallSizedGroup content={stories} />
-        <SmallSizedGroup content={discoverAmsterdam} gridSize={3} />
-        <InstagramSection />
-      </article>
+    <div className='w-screen h-full overflow-x-hidden px-4'>
+      {homeContents.map((section, index) => {
+        const Component = typeToComponent[section.type];
+        if (!Component) {
+          return null;
+        }
+
+        return (
+          <Component
+            key={index}
+            content={section.content}
+          />
+        );
+      })}
     </div>
   );
 };
